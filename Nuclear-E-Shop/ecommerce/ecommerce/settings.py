7a98a93a6d65c5d1d69f980a3080bcf25984642d
Abhishek Mishra <79761277+AbhiMishra-Now@users.noreply.github.com>
2023-04-16 01:12:30 +0530

    Update settings.py

--- Nuclear-E-Shop/ecommerce/ecommerce/settings.py
+++ Nuclear-E-Shop/ecommerce/ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#bm*obv%o#y5e=$s1dgg)*iv5=im)t_gh*z_jdzssl47ma1o79'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
